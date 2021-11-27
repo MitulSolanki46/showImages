99d1ad56789d10fa4d437f34aaba29552e468e92
mitulmsolanki <mitulsolanki566@gmail.com>
2021-11-27 22:33:09 +0530

    show me image when click

---
+++ hello.js
@@ -0,0 +1,9 @@
+document.querySelector(".btn").addEventListener("click", function() {
+    document.querySelector(".showImg").style.display = "flex";
+});
+document.querySelector(".btn1").addEventListener("click", function() {
+    document.querySelector(".showImg1").style.display = "flex";
+});
+document.querySelector(".btn2").addEventListener("click", function() {
+    document.querySelector(".showImg2").style.display = "flex";
+});
